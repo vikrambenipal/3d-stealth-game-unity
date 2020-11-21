08bbaec47b9e135a60c144ce3f6360e769c16d9f
vikrambenipal <vikrambenipal@gmail.com>
2020-11-21 00:44:08 -0500

    Guard controller

---
+++ Assets/Scripts/GuardController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardController : MonoBehaviour
+{
+    public Transform pathHolder;
+    public float guardSpeed;
+    public float guardWaitTime;
+    public float turnSpeed = 90;
+
+    void Start(){
+        int len = pathHolder.childCount;
+        Vector3[] waypoints = new Vector3[len];
+        for(int i = 0; i < len; ++i){
+            waypoints[i] = pathHolder.GetChild(i).position;
+            waypoints[i] = new Vector3(waypoints[i].x, transform.position.y, waypoints[i].z);
+        }
+        StartCoroutine(FollowPath(waypoints));
+    }
+
+
+    IEnumerator FollowPath(Vector3[] waypoints)
+    {
+
+        transform.position = waypoints[0];
+        int targetWaypointIndex = 1;
+        Vector3 targetWaypoint = waypoints[targetWaypointIndex];
+        transform.LookAt(targetWaypoint);
+        while (true)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetWaypoint, guardSpeed * Time.deltaTime);
+
+            if(transform.position == targetWaypoint)
+            {
+                targetWaypointIndex = (targetWaypointIndex + 1) % waypoints.Length;
+                targetWaypoint = waypoints[targetWaypointIndex];
+                yield return new WaitForSeconds(guardWaitTime);
+                yield return StartCoroutine(TurnToFace(targetWaypoint));
+            }
+            // yield for one frame at end of each iteration of while loop
+            // this is so the guard doesn't teleport to next position but moves there frame by frame 
+            yield return null;
+        }
+        
+    }
+
+    IEnumerator TurnToFace(Vector3 lookTarget)
+    {
+        // Angle guard needs on y-axis to face look target 
+        Vector3 dirLookTarget = (lookTarget - transform.position).normalized;
+        float targetAngle = 90 - Mathf.Atan2(dirLookTarget.z, dirLookTarget.x) * Mathf.Rad2Deg;
+
+        while (Mathf.DeltaAngle(transform.eulerAngles.y, targetAngle) > 0.05f)
+        {
+            //Debug.Log(transform.eulerAngles.y);
+            float angle = Mathf.MoveTowardsAngle(transform.eulerAngles.y, targetAngle, turnSpeed * Time.deltaTime);
+            transform.eulerAngles = Vector3.up * angle;
+            yield return null; 
+        }
+
+    }
+    
+    void OnDrawGizmos(){
+        Vector3 startPosition = pathHolder.GetChild(0).position;
+        Vector3 previousPosition = startPosition;
+        foreach(Transform waypoint in pathHolder){
+            Gizmos.DrawSphere(waypoint.position, .3f);
+            Gizmos.DrawLine(previousPosition, waypoint.position);
+            previousPosition = waypoint.position;
+        }
+        Gizmos.DrawLine(previousPosition, startPosition);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed; 
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // normalize since this is a direction 
+        Vector3 inputDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+
+        // if the vector has magnitude of 0 the player can't move 
+        float inputMagnitude = inputDirection.magnitude; 
+
+        // angle of rotation based on x and z axis 
+        float targetAngle =  Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg;
+
+        // rotate player along the y-axis 
+        transform.eulerAngles = Vector3.up * targetAngle;
+
+        // moves object automatically along the z-axis (direction * distance * magnitude) 
+        transform.Translate(transform.forward * moveSpeed * Time.deltaTime * inputMagnitude, Space.World);
+    }
+}
